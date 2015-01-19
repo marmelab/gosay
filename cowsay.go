5c9ad29b51646f574563408c35221fec37c5075b
ThieryMichel <thiery@marmelab.com>
2015-01-19 10:45:08 +0100

    properly display the cow

--- cowsay.go
+++ cowsay.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
+
+var eyes = "oo"
+var tongue = "  "
+var thoughts = "\\"
 
 func main() {
 	file, _ := ioutil.ReadFile("cows/default.cow")
+	cow := string(file)
+	cow = strings.Replace(cow, "$the_cow = <<\"EOC\";\n", "", 1)
+	cow = strings.Replace(cow, "\\\\", "\\", -1)
+	cow = strings.Replace(cow, "$eyes", eyes, 1)
+	cow = strings.Replace(cow, "$tongue", tongue, 1)
+	cow = strings.Replace(cow, "$thoughts", thoughts, 2)
+	cow = strings.Replace(cow, "\nEOC", "", 1)
 
-	fmt.Printf(string(file))
+	fmt.Printf(cow)
 }
